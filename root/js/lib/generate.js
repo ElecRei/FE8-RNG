6cae7842f10c5ac6bdaa6b5d5821a6bfaffe8a50
Ryan Furtado <ryanfurtado.elre@gmail.com>
2016-01-11 15:05:53 -0500

    File Upload

---
+++ root/js/lib/generate.js
@@ -0,0 +1,15 @@
+var body = document.getElementsByTagName('body');
+
+for (i = 0; i <= 0; i++) {
+    
+    body.innerHTML = 
+    "<pre>"+
+    "var prd01 = { prdName:\"Tropical Palm \" + productType[0],"+
+                  "prdImage: \"IMAGE GOES HERE\","+
+                  "prdPrice: \"90 \" + currency,"+
+                  "prdCode: \"190952CZ\""+
+                "};"+
+    "</pre>"
+    
+    
+}
---
+++ root/js/lib/list.js
@@ -0,0 +1,140 @@
+// 50 Chars Total
+
+
+            
+var charArray = [ char01,     // 0 - Eirika
+                  char02,     // 1 - Seth
+                  char03,     // 2 - Franz
+                  char04,     // 3 - Gilliam
+                  char05,     // 4 - Vanessa
+                  char06,     // 5 - Moulder
+                  char07,     // 6 - Ross
+                  char08,     // 7 - Garcia
+                  char09,     // 8 - Neimi
+                  char10,     // 9 - Colm
+                  char11,     // 10 - Artur
+                  char12,     // 11 - Lute
+                  char13,     // 12 - Natasha
+                  char14,     // 13 - Joshua
+                  char15,     // 14 - Ephraim 
+                  char16,     // 15 - Forde
+                  char17,     // 16 - Kyle
+                  char18,     // 17 - Orson
+                  char19,     // 18 - Tana
+                  char20,     // 19 - Amelia
+                  char21,     // 20 - Innes
+                  char22,     // 21 - Gerik
+                  char23,     // 22 - Tethys
+                  char24,     // 23 - Marisa
+                  char25,     // 24 - L'Arachel
+                  char26,     // 25 - Dozla
+                  char27,     // 26 - Saleh
+                  char28,     // 27 - Ewan
+                  char29,     // 28 - Cormag
+                  char30,     // 29 - Rennac
+                  char31,     // 30 - Dussel
+                  char32,     // 31 - Knoll
+                  char33,     // 32 - Myrrh
+                  char34,     // 33 - Syrene
+                  char35,     // 34 - Caellech
+                  char36,     // 35 - Riev
+                  char37,     // 36 - Isamire
+                  char38,     // 37 - Selena
+                  char39,     // 38 - Glen
+                  char40,     // 39 - Hayden
+                  char41,     // 40 - Valter
+                  char42,     // 41 - Fado
+                  char43,     // 42 - Lyon
+                  char44,     // 43 - Pablo
+                  char45,     // 44 - Klimt
+                  char46,     // 45 - Dara
+                  char47,     // 46 - Carlyle
+                  char48,     // 47 - Mansel
+                  char49,     // 48 - Vigarde
+                  char50      // 49 - Morva
+                ];
+
+
+
+// 64 Classes Total (including Enemy Classes)
+
+var classArray = [ class01,  // 00 - Lord
+                   class02,  // 01 - Recruit
+                   class03,  // 02 - Knight
+                   class04,  // 03 - Cavalier
+                   class05,  // 04 - General
+                   class06,  // 05 - Great Knight
+                   class07,  // 06 - Paladin
+                   class08,  // 07 - Archer
+                   class09,  // 08 - Mercenary
+                   class10,  // 09 - Sniper
+
+                   class11,  // 10 - Ranger
+                   class12,  // 11 - Hero
+                   class13,  // 12 - Journeyman
+                   class14,  // 13 - Fighter
+                   class15,  // 14 - Pirate
+                   class16,  // 15 - Warrior
+                   class17,  // 16 - Beserker
+                   class18,  // 17 - Mymirdom
+                   class19,  // 18 - Thief
+                   class20,  // 19 - Swordmaster
+
+                   class21,  // 20 - Assassin
+                   class22,  // 21 - Rogue
+                   class23,  // 22 - Pegasus Knight
+                   class24,  // 23 - Wyvern Rider
+                   class25,  // 24 - Falcon Knight
+                   class26,  // 25 - Wyvern Knight
+                   class27,  // 26 - Wyvern Lord
+                   class28,  // 27 - Troubadour
+                   class29,  // 28 - Cleric
+                   class30,  // 29 - Priest
+
+                   class31,  // 30 - Monk
+                   class32,  // 31 - Mage Knight
+                   class33,  // 32 - Valkeryie
+                   class34,  // 33 - Bishop
+                   class35,  // 34 - Sage
+                   class36,  // 35 - Pupil
+                   class37,  // 36 - Mage
+                   class38,  // 37 - Shaman
+                   class39,  // 38 - Mage Knight
+                   class40,  // 39 - Druid
+
+                   class41,  // 40 - Summoner
+                   class42,  // 41 - Dancer
+                   class43,  // 42 - Manakete
+                   class44,  // 43 - Solider
+                   class45,  // 44 - Bandit
+                   class46,  // 45 - Renevant
+                   class47,  // 46 - Entombed
+                   class48,  // 47 - Mogall
+                   class49,  // 48 - Arch Mogall
+                   class50,  // 49 - Bone Walker
+
+                   class51,  // 50 - Wight
+                   class52,  // 51 - Doog
+                   class53,  // 52 - Doog 2
+                   class54,  // 53 - Gargoyle
+                   class55,  // 54 - Deathgoyle
+                   class56,  // 55 - Tarvos
+                   class57,  // 56 - Maelduin
+                   class58,  // 57 - Bael
+                   class59,  // 58 - Elder Bael
+                   class60,  // 59 - Gorgon
+
+                   class61,  // 60 - Cyclops
+                   class62,  // 61 - Draco Zombie
+                   class63,  // 62 - Demon King
+                   class64,  // 63 - Great Lord
+
+                   // Bonus Classes
+
+                   class65,  // 64 - Queen
+                   class66,  // 65 - Pontifex
+                   class67,  // 66 - Phantom
+                   class68,  // 67 - Necromancer
+                   class69,  // 68 - Fleet
+                   class70   // 69 - Super Trainee
+                ];
